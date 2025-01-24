31166f502bef7afbc6b5d72955656910ece63e97
stouf <ouadad.mustapha01@gmail.com>
2025-01-24 09:50:30 +0100

    ajouter parti js apikey/urlkey

---
+++ app.js
@@ -0,0 +1,6 @@
+const apiKey ="f0997021dcb5911521e2ab4b21382604";
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
